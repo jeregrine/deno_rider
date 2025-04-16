489c111196d1b9ea773699a18f2e9fc9e1053c13
Jason Stiebs <stiebs.81@gmail.com>
2025-04-16 10:19:07 -0500

    adding older ubuntu

--- lib/deno_rider/native.ex
+++ lib/deno_rider/native.ex
@@ -1,8 +1,15 @@
 defmodule DenoRider.Native do
   version = Mix.Project.config()[:version]
 
+  use_old_glibc =
+    Application.compile_env(
+      :deno_rider,
+      :use_old_glibc,
+      System.get_env("DENO_RIDER_USE_OLD_GLIBC") in ["true", "1"]
+    )
+
   use RustlerPrecompiled,
-    base_url: "https://github.com/aglundahl/deno_rider/releases/download/v#{version}",
+    base_url: "https://github.com/jeregrine/deno_rider/releases/download/v#{version}",
     crate: "deno_rider",
     force_build: System.get_env("DENO_RIDER_BUILD") == "true",
     nif_versions: ["2.15"],
@@ -14,6 +21,9 @@ defmodule DenoRider.Native do
       "x86_64-pc-windows-msvc",
       "x86_64-unknown-linux-gnu"
     ],
+    variants: %{
+      "x86_64-unknown-linux-gnu" => [use_old_glibc: fn -> use_old_glibc end]
+    },
     version: version
 
   def start_runtime(_pid, _main_module_path), do: :erlang.nif_error(:nif_not_loaded)
